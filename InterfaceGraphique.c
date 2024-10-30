e14fe2640379d69d31f2370179d0c21928de0cf6
Vinh1VT <vinhvotuan@gmail.com>
2024-10-30 13:11:43 +0100

    debut de mouvement

--- InterfaceGraphique.c
+++ InterfaceGraphique.c
@@ -1,28 +1,43 @@
 #include "raylib.h"
-#include "rlgl.h"
 #include <stdlib.h>
+#include <math.h>
 
 int main(void)
 {
-    InitWindow(800, 450, "Simulation");
-    Camera2D Camera = {0};
+    const int ScreenWidth = 800;
+    const int ScreenHeight = 450;
+    
+    double StartTime = GetTime();
+    double actualTime = 0;
+    double deltaTime = 0.0;
+    float positionX = 0.0f;
+    float positionY = 0.0f;
+    float R = 70;
+
+    SetTargetFPS(60);
+    
+    InitWindow(ScreenWidth, ScreenHeight, "Simulation");
+    
+    
+    
+    
     while (!WindowShouldClose())
     {
+        positionX = ScreenWidth/2 + R * cos(((2*PI)/0.016)*deltaTime);
+        positionY = ScreenHeight/2 + R * sin(((2*PI)/0.016)*deltaTime);
+
         BeginDrawing();
-        BeginMode2D(Camera);
-        SetTargetFPS(10);
         ClearBackground(BLACK);
-        DrawCircle(400,225,50,YELLOW);
-        while(1){
-            rlPushMatrix();
-            rlScalef(10, 10, 10);
-            rlPopMatrix();
-            };
-
-
-        EndMode2D();
+        DrawCircle(ScreenWidth/2, ScreenHeight/2, 50, YELLOW);
+        
+        DrawCircle(positionX, positionY, 10, BLUE);
+        
         EndDrawing();
 
+        actualTime = GetTime();
+        deltaTime = actualTime - StartTime;
+        StartTime = actualTime;
+        
     }
 
     CloseWindow();
