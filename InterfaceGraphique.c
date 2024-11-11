049c46700602206f144b01596bd1cf1eb22082ba
unknown <alexis.lefevre2004@free.fr>
2024-11-11 14:54:25 +0100

    implémentation modèle v1

--- InterfaceGraphique.c
+++ InterfaceGraphique.c
@@ -1,8 +1,8 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <math.h>
-#include "Planete.h"
-
+#include "ModelePhysique.c"
+#include <stdio.h>
 
 #define PERIODE 5
 
@@ -10,12 +10,12 @@ int main(void)
 {
     const int ScreenWidth = 800;
     const int ScreenHeight = 450;
-
+/*
     Planet P;
     P.Pos_x = 0.0f;
     P.Pos_y = 0.0f;//toute cette partie est temporaire, faudra qu'on voit comment implémenter ça planete par planete
     float R = 100;
-
+*/
     double actualTime; //temps actuel
     double previousTime = GetTime(); //temps de début de la simulation
     double deltaTime = 0; //temps écoulé depuis le début de la simulation
@@ -29,23 +29,22 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-
+/*
         P.Pos_x = (float)ScreenWidth/2 + R * cosf(((2*PI)/PERIODE)*deltaTime);
         P.Pos_y = (float)ScreenHeight/2 + R * sinf(((2*PI)/PERIODE)*deltaTime); //On incrémente la position de oméga à chaque boucle (une boucle = 1 frame)
-
-
+*/
         BeginDrawing();
         ClearBackground(BLACK);
 
         DrawCircle(ScreenWidth/2, ScreenHeight/2, 25, YELLOW);
-
-        DrawCircle(P.Pos_x, P.Pos_y, 10, BLUE);
+        DrawCircle(Terre.Pos_x, Terre.Pos_y, 10, BLUE);
 
         EndDrawing();
 
         actualTime = GetTime();
         deltaTime += actualTime - previousTime;
         previousTime = actualTime;
+        GetNextPosition(&Terre, Soleil, deltaTime);
 
     }
 
--- ModelePhysique.c
+++ ModelePhysique.c
@@ -1,15 +1,56 @@
-
 #include <math.h>
 #include "Planete.h"
+#include "raylib.h"
 
+/* Quelques constantes*/
+float G = 1;
 
+Planet Soleil ={
+.Mass = 100,
+.Pos_x= 400,
+.Pos_y= 225,
+.Vitesse_x = 0,
+.Vitesse_y = 0};
 
+Planet Terre ={
+.Mass = 1,
+.Pos_x = 600,
+.Pos_y = 225,
+.Vitesse_x = 0,
+.Vitesse_y = -0.5};
 
-void GetNextPosition(Planet P, Planet S){
-
+/* Retourne le rayon entre P et T, peu importe l'ordre*/
+float get_R(Planet P,Planet T){
+    return sqrt((P.Pos_x - T.Pos_x)*(P.Pos_x - T.Pos_x) + (P.Pos_y - T.Pos_y)*(P.Pos_y - T.Pos_y));
 }
 
+/*Retourne l'angle entre P et T, en prenant P comme l'origine*/
+float get_theta(Planet P, Planet T){
+    float theta;
+    if ((P.Pos_x==T.Pos_x) && (T.Pos_y > P.Pos_y)){
+        theta=PI/2;
+    }
+    else if (P.Pos_x==T.Pos_x){
+        theta=-PI/2;
+    }
+    else{
+        theta = atanf((T.Pos_y-P.Pos_y)/(T.Pos_x-P.Pos_x));
+        if (T.Pos_x < P.Pos_x){
+            theta += PI;
+        }
+    }
+    return theta;
+}
 
+/* Calcule la prochaine position de P, en prenant en compte la gravité de S*/
+void GetNextPosition(Planet* P, Planet S,float deltatime){
+    float R = get_R(*P,S);
+    float theta = get_theta(*P,S);
 
+    P->Vitesse_x += (G * S.Mass * cosf(theta) * deltatime) / (R*R);
+    P->Vitesse_y += (G * S.Mass * sinf(theta) * deltatime) / (R*R);
 
-
+    P->Pos_x += P->Vitesse_x * deltatime;
+    P->Pos_y += P->Vitesse_y * deltatime;
+    return;
+}
