b09e3431160e0e0c3803ad227b3e606733e3f326
Vinh1VT <vinhvotuan@gmail.com>
2025-01-03 11:03:59 +0100

    changement de caméra

--- InterfaceGraphique.c
+++ InterfaceGraphique.c
@@ -36,7 +36,7 @@ int main(void)
     Planet Mars ={
     .Mass = 0.1,
     .Pos_x = Soleil.Pos_x+304,
-    .Pos_y = Soleil.Pos_y, 
+    .Pos_y = Soleil.Pos_y,
     .Vitesse_x = 0,
     .Vitesse_y = -47,
     .couleur = RED,
@@ -60,11 +60,11 @@ int main(void)
         .couleur = ORANGE,
         .Taille = 5
     };*/
-    
+
     FILE *f = fopen("./files/Calculs_Projet.csv","r");
     Planet* TableauPlanete = parse(f,Soleil.Pos_x,Soleil.Pos_y);
     fclose(f);
-    
+
     /*ListPlanet liste ={
         .start = &liste,
         .P = Terre,
@@ -72,7 +72,7 @@ int main(void)
         .couleur = BLUE,
         .Taille = 10
     };*/
-    
+
     ListPlanet* liste = newListe(TableauPlanete);
     for(int t = 1; t<8; t++){
         append(liste,&TableauPlanete[t]);
@@ -81,7 +81,7 @@ int main(void)
 
     InitWindow(ScreenWidth, ScreenHeight, "Simulation");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
-    
+
     //Caméra
     Camera2D camera = {0};
     camera.zoom = 1.0f;
@@ -97,7 +97,7 @@ int main(void)
     double previousTime = GetTime(); //temps de début de la simulation
     double deltaTime = 0; //temps écoulé depuis le début de la simulation
     float deltaTime2 = 0;
-    
+
     while (!WindowShouldClose())
     {
         if(i==TAILLETRACE) i=0;
@@ -107,38 +107,31 @@ int main(void)
 
         camera.offset = (Vector2){(float)ScreenWidth/2,(float)ScreenHeight/2};
 
-        //controle de la caméra 
+        //controle de la caméra
         if(true){
-        if(IsKeyDown(KEY_UP)) camera.target.y -= 2;
-        if(IsKeyDown(KEY_DOWN)) camera.target.y += 2;
-        if(IsKeyDown(KEY_RIGHT)) camera.target.x += 2;
-        if(IsKeyDown(KEY_LEFT)) camera.target.x -= 2;
+        if(IsKeyDown(KEY_UP)) camera.target.y -= 2 + (1-camera.zoom);
+        if(IsKeyDown(KEY_DOWN)) camera.target.y += 2+ (1-camera.zoom);
+        if(IsKeyDown(KEY_RIGHT)) camera.target.x += 2+ (1-camera.zoom);
+        if(IsKeyDown(KEY_LEFT)) camera.target.x -= 2+ (1-camera.zoom);
         if(IsKeyDown(KEY_ENTER)) camera.target = (Vector2){Soleil.Pos_x,Soleil.Pos_y};
         if(IsKeyDown(KEY_RIGHT_SHIFT)) camera.zoom = 1;
         if(IsKeyPressed(KEY_KP_ADD)) camera.zoom += 0.1f;
         if(IsKeyPressed(KEY_KP_SUBTRACT)) camera.zoom -= 0.1f;
         if(camera.zoom<0.1f) camera.zoom=0.1f;
         if(IsKeyPressed(KEY_T)) afficheTrace = !afficheTrace;
-        
-        /*if(IsKeyDown(KEY_ONE)){
-            camera.target = (Vector2){Mercure.Pos_x ,Mercure.Pos_y };
+
+        for(int c = 0; c<=8;c++){
+            if(IsKeyDown(49+c)){
+                camera.target = (Vector2){TableauPlanete[c].Pos_x, TableauPlanete[c].Pos_y};
+            }
         }
-        if(IsKeyDown(KEY_TWO)){
-            camera.target = (Vector2){Venus.Pos_x,Venus.Pos_y};
-        }
-        if(IsKeyDown(KEY_THREE)){
-            camera.target = (Vector2){Terre.Pos_x ,Terre.Pos_y };
-        }
-        if(IsKeyDown(KEY_FOUR)){
-            camera.target = (Vector2){Mars.Pos_x ,Mars.Pos_y };
-        }*/
         }
 
-        ListPlanet* ptr = liste;
-        while(ptr !=NULL){
+        //ListPlanet* ptr = liste;
+        /*while(ptr !=NULL){
             printf("%s\n",ptr->P->Nom);
             ptr = ptr->suivant;
-        }    
+            }  */
 
 
         BeginDrawing();
@@ -152,7 +145,7 @@ int main(void)
         DrawAll(liste);
 
 
-        
+
         //Trace
         if(afficheTrace) DrawTrace(liste);
         //Trajectoire idéale
@@ -171,7 +164,7 @@ int main(void)
         deltaTime2 += actualTime - previousTime;
         previousTime = actualTime;
 
-        
+
 
         UpdateTrace(liste, i);
         i++;
