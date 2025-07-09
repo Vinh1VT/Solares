42ac6cafe3a8656e893258611ad34f04f6998f00
Vinh1VT <vinhvotuan@gmail.com>
2025-07-09 11:02:46 +0200

    The sun is also in the CSV now (at last)

--- InterfaceGraphique.c
+++ InterfaceGraphique.c
@@ -15,26 +15,26 @@ int main(void)
 
 
 
-    Planet Soleil ={
+    /*Planet Soleil ={
     .Mass = 1E25,
     .Pos_x= (float)ScreenWidth/2,
     .Pos_y= (float)ScreenHeight/2,
     .Vitesse_x = 0,
     .Vitesse_y = 0
-    };
+    };*/
 
     FILE *f;
     if(!(f = fopen("./files/Calculs_Projet.csv","r"))){
         fprintf(stderr,"erreur d'ouverture du fichier");
     };
-    Planet* TableauPlanete = parse(f,Soleil.Pos_x,Soleil.Pos_y);
+    Planet* TableauPlanete = parse(f,(float)ScreenWidth/2,(float)ScreenHeight/2);
     fclose(f);
 
-    ListPlanet* liste = newListe(TableauPlanete, true);
-    for(int t = 1; t<8; t++){
+    ListPlanet* liste = newListe(TableauPlanete+1, true);
+    for(int t = 2; t<9; t++){
         append(liste,&TableauPlanete[t],true);
     }
-    add_asteroide(2500,500,800,&Soleil,liste);
+    add_asteroide(2500,500,800,&TableauPlanete[0],liste);
 
     InitWindow(ScreenWidth, ScreenHeight, "Solares");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
@@ -57,8 +57,7 @@ int main(void)
     //Tableau des bits qui marquent l'état des planetes (affiches ou non)
     int PlaneteAffichee[8] = {1,1,1,1,1,1,1,1};
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()){
         if(i==TAILLETRACE) i=0;
 
         ScreenHeight = GetScreenHeight();
@@ -68,34 +67,34 @@ int main(void)
 
         //controle de la caméra
         {
-        if(IsKeyDown(KEY_UP)) camera.target.y -= 2 * (1/camera.zoom);
-        if(IsKeyDown(KEY_DOWN)) camera.target.y += 2 * (1/camera.zoom);
-        if(IsKeyDown(KEY_RIGHT)) camera.target.x += 2 * (1/camera.zoom);
-        if(IsKeyDown(KEY_LEFT)) camera.target.x -= 2 * (1/camera.zoom);
-        if(IsKeyDown(KEY_ENTER)) camera.target = (Vector2){Soleil.Pos_x,Soleil.Pos_y};
-        if(IsKeyDown(KEY_RIGHT_SHIFT)) camera.zoom = 1;
-        if(IsKeyPressed(KEY_KP_ADD)) camera.zoom += 0.1f;
-        if(IsKeyPressed(KEY_KP_SUBTRACT)) camera.zoom -= 0.1f;
-        if(camera.zoom<0.1f) camera.zoom=0.1f;
-        if(IsKeyPressed(KEY_T)) afficheTrace = !afficheTrace;
-        if(IsKeyPressed(KEY_N)) afficheNom = !afficheNom;
+            if(IsKeyDown(KEY_UP)) camera.target.y -= 2 * (1/camera.zoom);
+            if(IsKeyDown(KEY_DOWN)) camera.target.y += 2 * (1/camera.zoom);
+            if(IsKeyDown(KEY_RIGHT)) camera.target.x += 2 * (1/camera.zoom);
+            if(IsKeyDown(KEY_LEFT)) camera.target.x -= 2 * (1/camera.zoom);
+            if(IsKeyDown(KEY_ENTER)) camera.target = (Vector2){TableauPlanete[0].Pos_x,TableauPlanete[0].Pos_y};
+            if(IsKeyDown(KEY_RIGHT_SHIFT)) camera.zoom = 1;
+            if(IsKeyPressed(KEY_KP_ADD)) camera.zoom += 0.1f;
+            if(IsKeyPressed(KEY_KP_SUBTRACT)) camera.zoom -= 0.1f;
+            if(camera.zoom<0.1f) camera.zoom=0.1f;
+            if(IsKeyPressed(KEY_T)) afficheTrace = !afficheTrace;
+            if(IsKeyPressed(KEY_N)) afficheNom = !afficheNom;
 
-        for(int c = 0; c<8;c++){
-            if(IsKeyDown(49+c)){
-                camera.target = (Vector2){TableauPlanete[c].Pos_x, TableauPlanete[c].Pos_y};
-            }
-        }
-        for(int c = 0; c<8;c++){
-            if(IsKeyPressed(321+c)){
-                if(PlaneteAffichee[c]){
-                    liste = RemovePlanet(TableauPlanete[c].Nom, liste);
-                    PlaneteAffichee[c] = 0;
-                } else{
-                    append(liste,&TableauPlanete[c],true);
-                    PlaneteAffichee[c] = 1;
+            for(int c = 0; c<8;c++){
+                if(IsKeyDown(49+c)){
+                    camera.target = (Vector2){TableauPlanete[c+1].Pos_x, TableauPlanete[c+1].Pos_y};
                 }
             }
-        }
+            for(int c = 0; c<8;c++){
+                if(IsKeyPressed(321+c)){
+                    if(PlaneteAffichee[c+1]){
+                        liste = RemovePlanet(TableauPlanete[c+1].Nom, liste);
+                        PlaneteAffichee[c+1] = 0;
+                    } else{
+                        append(liste,&TableauPlanete[c+1],true);
+                        PlaneteAffichee[c] = 1;
+                    }
+                }
+            }
         }
 
 
@@ -104,8 +103,7 @@ int main(void)
 
         BeginMode2D(camera);
 
-
-        DrawCircleGradient(Soleil.Pos_x, Soleil.Pos_y, 20, YELLOW, ORANGE);
+        DrawCircleGradient(TableauPlanete[0].Pos_x,TableauPlanete[0].Pos_y,TableauPlanete[0].Taille,YELLOW,ORANGE);
         DrawAll(liste);
 
 
@@ -128,10 +126,10 @@ int main(void)
 
         UpdateTrace(liste, i);
         i++;
-        GetNextVitesseAll(liste,&Soleil,deltaTime);
+        GetNextVitesseAll(liste,&TableauPlanete[0],deltaTime);
 
         GetNextPositionAll(liste, deltaTime);
-        //DrawFPS(Soleil.Pos_x,Soleil.Pos_y);
+        //DrawFPS(TableauPlanete[0].Pos_x,TableauPlanete[0].Pos_y);
     }
 
     CloseWindow();
